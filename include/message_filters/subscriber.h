667f1d83cc0faaf95d674db0b56c9187b4ccf0e8
Audrow Nash <audrow@hey.com>
2021-05-18 12:55:12 -0700

    Log error instead of warning
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- include/message_filters/subscriber.h
+++ include/message_filters/subscriber.h
@@ -107,7 +107,9 @@ public:
     rclcpp::SubscriptionOptions options)
   {
     (void) options;
-    RCLCPP_WARN(node->get_logger(), "SubscriberBase::subscribe with four arguments has not been overridden");
+    RCLCPP_ERROR(
+      node->get_logger(),
+      "SubscriberBase::subscribe with four arguments has not been overridden");
     this->subscribe(node, topic, qos);
   }
 
