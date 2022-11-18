0b325d6a502f78069267d18f1f66fa052e262f8b
Audrow Nash <audrow@openrobotics.org>
2022-11-18 05:41:36 -0800

    [rolling] Update maintainers - 2022-11-07 (#85)

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['message_filters'],
     package_dir={'': 'src'},
-    requires=['genmsg', 'genpy', 'roslib', 'rospkg']
+    requires=['genmsg', 'genpy', 'roslib', 'rospkg'],
+    maintainer='Geoffrey Biggs',
+    maintainer_email='geoff@openrobotics.org',
 )
 
 setup(**d)
