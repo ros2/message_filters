49d9a33ecbdac3466ebb2fa941a37dcbcf86992e
Yuyuan Yuan <az6980522@gmail.com>
2025-03-04 22:01:16 +0800

    fix: add `rclcpp::shutdown` (#167)
    (cherry picked from commit dfe5dde18465bac2fdf9e485c1df84bbf2f46332)

--- test/test_synchronizer.cpp
+++ test/test_synchronizer.cpp
@@ -451,7 +451,9 @@ TEST(Synchronizer, add9)
 int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  auto ret = RUN_ALL_TESTS();
+  rclcpp::shutdown();
+  return ret;
 }
 
 
