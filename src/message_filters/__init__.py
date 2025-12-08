1f2c4e881cd7458448cc56caf933995127e03657
Pavel Esipov <38457822+EsipovPA@users.noreply.github.com>
2025-12-08 13:48:11 +0300

    Fix rclpty typo in src/message_filters/__init__.py (#236)
    Signed-off-by: EsipovPA <esipov.p@mail.ru>

--- src/message_filters/__init__.py
+++ src/message_filters/__init__.py
@@ -44,10 +44,9 @@ from typing import Type, Union
 from rclpy.clock import ROSClock
 from rclpy.duration import Duration
 from rclpy.logging import LoggingSeverity
-from rclpty.node import Node
+from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from rclpy.time import Time
-from rclpy.type_support import MsgT
 
 
 class SimpleFilter(object):
@@ -84,7 +83,7 @@ class Subscriber(SimpleFilter):
     def __init__(
         self,
         node: Node,
-        msg_type: Type[MsgT],
+        msg_type: Type,
         topic: str,
         qos_profile: Union[QoSProfile, int] = QoSProfile(depth=10),
     ) -> None:
