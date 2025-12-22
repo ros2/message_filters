77f911d9714dc52e2f0b4922c2fa50f489df195b
Pavel Esipov <38457822+EsipovPA@users.noreply.github.com>
2025-12-22 18:50:51 +0300

    #200 fix inconsistensy between cpp and python exact time synchronizer impl (#238)
    Signed-off-by: EsipovPA <esipov.p@mail.ru>
    Signed-off-by: Dominik <45536968+authaldo@users.noreply.github.com>
    Co-authored-by: Dominik <45536968+authaldo@users.noreply.github.com>
    (cherry picked from commit 8cf647e1cc1d88ed992b7019212c5156ab2c3885)

--- test/test_time_synchronizer.py
+++ test/test_time_synchronizer.py
@@ -41,7 +41,7 @@
 #
 #    wide.registerCallback(boost::bind(&PersonDataRecorder::wideCB, this, _1, _2, _3, _4));
 
-import random
+import functools
 import unittest
 
 from builtin_interfaces.msg import Time as TimeMsg
@@ -66,47 +66,111 @@ class MockFilter(SimpleFilter):
 
 class TestDirected(unittest.TestCase):
 
-    def cb_collector_2msg(self, msg1, msg2):
-        self.collector.append((msg1, msg2))
+    @staticmethod
+    def collector_callback(msg1, msg2, collector):
+        collector.append((msg1, msg2))
 
-    def test_synchronizer(self):
-        m0 = MockFilter()
-        m1 = MockFilter()
-        ts = TimeSynchronizer([m0, m1], 1)
-        ts.registerCallback(self.cb_collector_2msg)
-
-        if 0:
-            # Simple case, pairs of messages, make sure that they get combined
-            for t in range(10):
-                self.collector = []
-                msg0 = MockMessage(t, 33)
-                msg1 = MockMessage(t, 34)
-                m0.signalMessage(msg0)
-                self.assertEqual(self.collector, [])
-                m1.signalMessage(msg1)
-                self.assertEqual(self.collector, [(msg0, msg1)])
-
-        # Scramble sequences of length N.
-        # Make sure that TimeSequencer recombines them.
-        random.seed(0)
+    def test_time_synchronizer_queue_lentgth(self):
         for N in range(1, 10):
+            seq0 = [MockMessage(t, 0) for t in range(N)]
+            seq1 = [MockMessage(t, 0) for t in range(N)]
+
             m0 = MockFilter()
             m1 = MockFilter()
-            seq0 = [MockMessage(t, random.random()) for t in range(N)]
-            seq1 = [MockMessage(t, random.random()) for t in range(N)]
-            # random.shuffle(seq0)
             ts = TimeSynchronizer([m0, m1], N)
-            ts.registerCallback(self.cb_collector_2msg)
-            self.collector = []
-            for msg in random.sample(seq0, N):
+
+            collector = []
+            ts.registerCallback(
+                functools.partial(
+                    self.collector_callback,
+                    collector=collector,
+                )
+            )
+
+            for msg in seq0:
                 m0.signalMessage(msg)
-            self.assertEqual(self.collector, [])
-            for msg in random.sample(seq1, N):
+            self.assertEqual(collector, [])
+            for msg in seq1:
                 m1.signalMessage(msg)
-            self.assertEqual(set(self.collector), set(zip(seq0, seq1)))
+            self.assertEqual(set(collector), set(zip(seq0, seq1)))
+
+    def test_time_synchronizer_drop_old_messages(self):
+        collector = []
+
+        filter_0 = MockFilter()
+        filter_1 = MockFilter()
+        ts = TimeSynchronizer([filter_0, filter_1], 10)
+        ts.registerCallback(
+            functools.partial(
+                self.collector_callback,
+                collector=collector,
+            )
+        )
+
+        t1 = 0
+        t2 = 1
+
+        x0 = MockMessage(t1, 1)
+        x1 = MockMessage(t1, 2)
+
+        y0 = MockMessage(t2, 1)
+        y1 = MockMessage(t2, 2)
+
+        filter_0.signalMessage(x0)
+        assert len(collector) == 0
+
+        filter_1.signalMessage(y1)
+        assert len(collector) == 0
+
+        filter_0.signalMessage(y0)
+        assert len(collector) == 1
+        assert collector[0] == (y0, y1)
+
+        filter_1.signalMessage(x1)
+        assert len(collector) == 1
+        assert collector[0] == (y0, y1)
+
+    def test_time_synchronizer_shifted_time_signalling(self):
+        collector = []
+
+        filter_0 = MockFilter()
+        filter_1 = MockFilter()
+        ts = TimeSynchronizer([filter_0, filter_1], 10)
+        ts.registerCallback(
+            functools.partial(
+                self.collector_callback,
+                collector=collector,
+            )
+        )
+
+        t1 = 0
+        t2 = 1
+
+        x0 = MockMessage(t1, 1)
+        x1 = MockMessage(t1, 2)
+
+        y0 = MockMessage(t2, 1)
+        y1 = MockMessage(t2, 2)
+
+        filter_0.signalMessage(x0)
+        assert len(collector) == 0
+
+        filter_0.signalMessage(y0)
+        assert len(collector) == 0
+
+        filter_1.signalMessage(x1)
+        assert len(collector) == 1
+        assert collector[0] == (x0, x1)
+
+        filter_1.signalMessage(y1)
+        assert len(collector) == 2
+        assert collector[0] == (x0, x1)
+        assert collector[1] == (y0, y1)
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(TestDirected('test_synchronizer'))
+    suite.addTest(TestDirected('test_time_synchronizer_queue_lentgth'))
+    suite.addTest(TestDirected('test_time_synchronizer_drop_old_messages'))
+    suite.addTest(TestDirected('test_time_synchronizer_shifted_time_signalling'))
     unittest.TextTestRunner(verbosity=2).run(suite)
