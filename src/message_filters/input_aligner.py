61a415176de3ab157ccef771a1151720eaa53f18
YANG Zhenfei <yangzhenfei0@gmail.com>
2026-04-17 19:48:02 +0800

    refactor(python): use Queue storage for events

--- src/message_filters/input_aligner.py
+++ src/message_filters/input_aligner.py
@@ -1,6 +1,6 @@
 from bisect import insort_right
 from dataclasses import dataclass
-from queue import PriorityQueue
+from queue import Queue
 import threading
 
 from builtin_interfaces.msg import Time as TimeMsg
@@ -42,7 +42,7 @@ def _ros_max_time():
 
 class _EventQueue:
     def __init__(self):
-        self.events = PriorityQueue()
+        self.events = Queue()
         self.next_ts = _ros_max_time()
         self.period = Duration(seconds=0)
         self.active = False
